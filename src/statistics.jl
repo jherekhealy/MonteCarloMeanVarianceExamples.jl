68484e10c24d88a7ef5373150424e6b6279f4545
Jherek Healy <jherekhealy@protonmail.com>
2022-08-14 11:26:02 +0200

    format code
    Change-Id: Ifa9c2e3219e4cc88a6fc6ca9c35adf741c6e1efd

--- src/statistics.jl
+++ src/statistics.jl
@@ -1,18 +1,30 @@
-export SimulationStatistics, ChanLewis, Kahan, Ling, Klein, Naive, recordValue, lastMeasure,variance, sampleVariance, mean, standardError
+export SimulationStatistics,
+    ChanLewis,
+    Kahan,
+    Ling,
+    Klein,
+    Naive,
+    recordValue,
+    lastMeasure,
+    variance,
+    sampleVariance,
+    mean,
+    standardError
 
 abstract type StatisticsAlgorithm end
 
 mutable struct SimulationMeasure{T}
-	index::Int
-	mean::T
-	variance::T
+    index::Int
+    mean::T
+    variance::T
 end
 
 mutable struct SimulationStatistics{T}
     algo::StatisticsAlgorithm
-	table::AbstractVector{SimulationMeasure{T}}
-	nextSampleSize::Int
-	SimulationStatistics{T}(salgo::StatisticsAlgorithm) where {T} = (reset(salgo); new(salgo, T[],  128 - 1))
+    table::AbstractVector{SimulationMeasure{T}}
+    nextSampleSize::Int
+    SimulationStatistics{T}(salgo::StatisticsAlgorithm) where {T} =
+        (reset(salgo); new(salgo, T[], 128 - 1))
 
 end
 
@@ -20,67 +32,69 @@ Base.broadcastable(ss::SimulationStatistics) = Ref(ss)
 
 
 mutable struct Ling{T} <: StatisticsAlgorithm
-	counter::Int
-	A::T
-	Q::         T
-	Ling{T}() where {T} = new(0, zero(T), zero(T))
+    counter::Int
+    A::T
+    Q::T
+    Ling{T}() where {T} = new(0, zero(T), zero(T))
 end
 
 mutable struct ChanLewis{T} <: StatisticsAlgorithm
-	counter::Int
-	S::T
-	A::T
-	Q::         T
-	ChanLewis{T}() where {T} = new(0, zero(T),zero(T), zero(T))
+    counter::Int
+    S::T
+    A::T
+    Q::T
+    ChanLewis{T}() where {T} = new(0, zero(T), zero(T), zero(T))
 end
 
 
 mutable struct Naive{U} <: StatisticsAlgorithm
-	counter::Int
-	A::U
-	Q::         U
-	K::U
-	useShift::Bool
-	Naive{U}(;useShift::Bool=false) where {U} = new(0, zero(U), zero(U), zero(U),useShift)
+    counter::Int
+    A::U
+    Q::U
+    K::U
+    useShift::Bool
+    Naive{U}(; useShift::Bool = false) where {U} =
+        new(0, zero(U), zero(U), zero(U), useShift)
 end
 
 mutable struct Kahan{T,U} <: StatisticsAlgorithm
-	parent::T
-	Astar::U
-	Qstar::U
-	Kahan{U}(parent::T) where {T,U} = new{T,U}(parent, zero(U),zero(U))
+    parent::T
+    Astar::U
+    Qstar::U
+    Kahan{U}(parent::T) where {T,U} = new{T,U}(parent, zero(U), zero(U))
 end
 
 
 mutable struct Klein{T,U} <: StatisticsAlgorithm
-	parent::T
-	S::U
-	CS::U
-	CCS::U
-	S2::U
-	CS2::U
-	CCS2::U
-	Klein{U}(parent::T) where {T,U} = new{T,U}(parent,zero(U), zero(U),zero(U),zero(U),zero(U),zero(U))
+    parent::T
+    S::U
+    CS::U
+    CCS::U
+    S2::U
+    CS2::U
+    CCS2::U
+    Klein{U}(parent::T) where {T,U} =
+        new{T,U}(parent, zero(U), zero(U), zero(U), zero(U), zero(U), zero(U))
 end
 
 
 function variance(sm::SimulationMeasure{T}) where {T}
-	return sm.variance
+    return sm.variance
 end
 
 function sampleVariance(sm::SimulationMeasure{T}) where {T}
-	return sm.variance * sm.index / (sm.index-1)
+    return sm.variance * sm.index / (sm.index - 1)
 end
 
 function standardError(sm::SimulationMeasure{T}) where {T}
-	return sqrt(sampleVariance(sm) / sm.index)
+    return sqrt(sampleVariance(sm) / sm.index)
 end
 function mean(sm::SimulationMeasure{T}) where {T}
-	return sm.mean
+    return sm.mean
 end
 
 function setNextSampleSize(ss::SimulationStatistics{T}) where {T}
-	ss.nextSampleSize = size
+    ss.nextSampleSize = size
 end
 
 function lastMeasure(ss::SimulationStatistics{T}) where {T}
@@ -89,188 +103,198 @@ end
 
 function updateStatistics(ss::Ling{U}, x) where {U}
     ss.counter += 1
-	ss.Q += ((ss.counter-1) * (x - ss.A)^2) / (ss.counter)
-	ss.A += (x - ss.A) / (ss.counter)
+    ss.Q += ((ss.counter - 1) * (x - ss.A)^2) / (ss.counter)
+    ss.A += (x - ss.A) / (ss.counter)
 end
 
 
 function updateStatistics(s::Kahan{Ling{T},U}, x) where {U,T}
-	ss = s.parent
+    ss = s.parent
     ss.counter += 1
-	z = ((ss.counter-1) * (x - ss.A)^2) / (ss.counter)
-	y = z-s.Qstar; t = ss.Q + y
-    s.Qstar = (t-ss.Q) - y
-	ss.Q = t
-	z = (x - ss.A)/ss.counter
-	y = z - s.Astar; t = ss.A + y
-	s.Astar = (t-ss.A) - y
-	ss.A = t
+    z = ((ss.counter - 1) * (x - ss.A)^2) / (ss.counter)
+    y = z - s.Qstar
+    t = ss.Q + y
+    s.Qstar = (t - ss.Q) - y
+    ss.Q = t
+    z = (x - ss.A) / ss.counter
+    y = z - s.Astar
+    t = ss.A + y
+    s.Astar = (t - ss.A) - y
+    ss.A = t
 end
 
 
 function updateStatistics(s::Klein{Ling{T},U}, x) where {U,T}
-   ss = s.parent
-   ss.counter += 1
-	z = ((ss.counter-1) *(x-ss.A)*(x-ss.A))/(ss.counter)
-	t = s.S2 + z
-	c =  abs(s.S2) >= abs(z) ? (s.S2 - t) + z :  (z - t) + s.S2
+    ss = s.parent
+    ss.counter += 1
+    z = ((ss.counter - 1) * (x - ss.A) * (x - ss.A)) / (ss.counter)
+    t = s.S2 + z
+    c = abs(s.S2) >= abs(z) ? (s.S2 - t) + z : (z - t) + s.S2
     s.S2 = t
-            t = s.CS2 + c
-          cc = abs(s.CS2) >= abs(c) ? (s.CS2 - t) + c :   (c - t) + s.CS2
-            s.CS2 = t
-            s.CCS2 += cc
-            ss.Q = (s.S2 + s.CS2 + s.CCS2)
+    t = s.CS2 + c
+    cc = abs(s.CS2) >= abs(c) ? (s.CS2 - t) + c : (c - t) + s.CS2
+    s.CS2 = t
+    s.CCS2 += cc
+    ss.Q = (s.S2 + s.CS2 + s.CCS2)
 
-	z = (x - ss.A)/ss.counter
-	t = s.S + z
-	c = abs(s.S) >= abs(z) ? (s.S - t) + z :  (z - t) + s.S
-	s.S = t
+    z = (x - ss.A) / ss.counter
+    t = s.S + z
+    c = abs(s.S) >= abs(z) ? (s.S - t) + z : (z - t) + s.S
+    s.S = t
     t = s.CS + c
-	cc = abs(s.CS) >= abs(c) ?  (s.CS - t) + c :   (c - t) + s.CS
-	s.CS = t
-	s.CCS += cc
-	ss.A = (s.S + s.CS + s.CCS)
+    cc = abs(s.CS) >= abs(c) ? (s.CS - t) + c : (c - t) + s.CS
+    s.CS = t
+    s.CCS += cc
+    ss.A = (s.S + s.CS + s.CCS)
 end
 
 
 function setShiftFromFirstObservation(ss::Naive{T}, x) where {T}
-	if ss.useShift && ss.counter == 1
-		ss.K = x
-	end
+    if ss.useShift && ss.counter == 1
+        ss.K = x
+    end
 end
 
 
 
 function updateStatistics(s::Kahan{Naive{T},U}, x) where {U,T}
-	ss = s.parent
+    ss = s.parent
     ss.counter += 1
-	setShiftFromFirstObservation(ss, x)
-	z = (convert(U,x)-ss.K)^2
-	y = z-s.Qstar; t = ss.Q + y
-    s.Qstar = (t-ss.Q) - y
-	ss.Q = t
-	z = convert(U,x)
-	y = z - s.Astar; t = ss.A + y
-	s.Astar = (t-ss.A) - y
-	ss.A = t
+    setShiftFromFirstObservation(ss, x)
+    z = (convert(U, x) - ss.K)^2
+    y = z - s.Qstar
+    t = ss.Q + y
+    s.Qstar = (t - ss.Q) - y
+    ss.Q = t
+    z = convert(U, x)
+    y = z - s.Astar
+    t = ss.A + y
+    s.Astar = (t - ss.A) - y
+    ss.A = t
 end
 
 
 #function updateStatistics(s::NaiveKlein{U}, x) where {U}
 function updateStatistics(s::Klein{Naive{T},U}, x) where {U,T}
-	ss = s.parent
+    ss = s.parent
     ss.counter += 1
-	setShiftFromFirstObservation(ss, x)
-	z = convert(U,x-ss.K)^2 #the raw square implies a loss of accuracy
-	t = s.S2 + z
-	c =  abs(s.S2) >= abs(z) ? (s.S2 - t) + z :  (z - t) + s.S2
+    setShiftFromFirstObservation(ss, x)
+    z = convert(U, x - ss.K)^2 #the raw square implies a loss of accuracy
+    t = s.S2 + z
+    c = abs(s.S2) >= abs(z) ? (s.S2 - t) + z : (z - t) + s.S2
     s.S2 = t
-            t = s.CS2 + c
-          cc = abs(s.CS2) >= abs(c) ? (s.CS2 - t) + c :   (c - t) + s.CS2
-		s.CS2 = t
-            s.CCS2 += cc
-            ss.Q = (s.S2 + s.CS2 + s.CCS2)
+    t = s.CS2 + c
+    cc = abs(s.CS2) >= abs(c) ? (s.CS2 - t) + c : (c - t) + s.CS2
+    s.CS2 = t
+    s.CCS2 += cc
+    ss.Q = (s.S2 + s.CS2 + s.CCS2)
 
-	z = x
-	t = s.S + z
-	c = abs(s.S) >= abs(z) ? (s.S - t) + z :  (z - t) + s.S
-	s.S = t
+    z = x
+    t = s.S + z
+    c = abs(s.S) >= abs(z) ? (s.S - t) + z : (z - t) + s.S
+    s.S = t
     t = s.CS + c
-	cc = abs(s.CS) >= abs(c) ?  (s.CS - t) + c :   (c - t) + s.CS
-	s.CS = t
-	s.CCS += cc
-	ss.A = (s.S + s.CS + s.CCS)
+    cc = abs(s.CS) >= abs(c) ? (s.CS - t) + c : (c - t) + s.CS
+    s.CS = t
+    s.CCS += cc
+    ss.A = (s.S + s.CS + s.CCS)
 end
 
 function lastMeasure(::Type{T}, ss::Ling{U})::SimulationMeasure{T} where {T,U}
-    return SimulationMeasure(ss.counter, convert(T,ss.A), convert(T,ss.Q/ss.counter))
+    return SimulationMeasure(ss.counter, convert(T, ss.A), convert(T, ss.Q / ss.counter))
 end
 
 function updateStatistics(ss::Naive{U}, x::T) where {U,T}
     ss.counter += 1
-	setShiftFromFirstObservation(ss, x)
-	ss.Q += (convert(U,x)-ss.K)^2
-	ss.A += convert(U,x)
+    setShiftFromFirstObservation(ss, x)
+    ss.Q += (convert(U, x) - ss.K)^2
+    ss.A += convert(U, x)
 end
 
 function updateStatistics(ss::ChanLewis{U}, x::T) where {U,T}
     ss.counter += 1
-	ss.Q += ((ss.counter-1) * (x - ss.A)^2) / (ss.counter)
-	ss.S += x
-	ss.A = ss.S/ss.counter
+    ss.Q += ((ss.counter - 1) * (x - ss.A)^2) / (ss.counter)
+    ss.S += x
+    ss.A = ss.S / ss.counter
 end
 
 
 function updateStatistics(s::Kahan{ChanLewis{T},U}, x) where {U,T}
-	ss = s.parent
+    ss = s.parent
     ss.counter += 1
-    z = ((ss.counter-1) * (x - ss.A)^2) / (ss.counter)
-    y = z-s.Qstar; t = ss.Q + y
-	s.Qstar = (t-ss.Q) - y
-	ss.Q = t
-	y = x - s.Astar; t = ss.S + y
-	s.Astar = (t-ss.S) - y
-	ss.S = t
-	ss.A = ss.S/ss.counter
+    z = ((ss.counter - 1) * (x - ss.A)^2) / (ss.counter)
+    y = z - s.Qstar
+    t = ss.Q + y
+    s.Qstar = (t - ss.Q) - y
+    ss.Q = t
+    y = x - s.Astar
+    t = ss.S + y
+    s.Astar = (t - ss.S) - y
+    ss.S = t
+    ss.A = ss.S / ss.counter
 end
 
 
 function lastMeasure(::Type{T}, ss::Union{Kahan,Klein})::SimulationMeasure{T} where {T}
-  return lastMeasure(T, ss.parent)
+    return lastMeasure(T, ss.parent)
 end
 function lastMeasure(::Type{T}, ss::ChanLewis)::SimulationMeasure{T} where {T}
-	return SimulationMeasure(ss.counter, convert(T,ss.A), convert(T,ss.Q/ss.counter))
+    return SimulationMeasure(ss.counter, convert(T, ss.A), convert(T, ss.Q / ss.counter))
 end
 
 function Base.reset(ss::ChanLewis{T}) where {T}
     ss.counter = 0
-	ss.A = zero(T)
-	ss.Q = zero(T)
-	ss.S = zero(T)
+    ss.A = zero(T)
+    ss.Q = zero(T)
+    ss.S = zero(T)
 end
 
 function Base.reset(s::Kahan{U,T}) where {U,T}
-	reset(s.parent)
-	s.Qstar = zero(T)
-	s.Astar = zero(T)
+    reset(s.parent)
+    s.Qstar = zero(T)
+    s.Astar = zero(T)
 end
 
 function Base.reset(ss::Union{Ling{T},Naive{T}}) where {T}
     ss.counter = 0
-	ss.A = zero(T)
-	ss.Q = zero(T)
+    ss.A = zero(T)
+    ss.Q = zero(T)
 end
 
 function Base.reset(ss::Klein{U,T}) where {U,T}
-	reset(ss.parent)
-    ss.S = zero(T); ss.CS = zero(T); ss.CCS = zero(T)
-	ss.S2 = zero(T); ss.CS2 = zero(T); ss.CCS2 = zero(T)
+    reset(ss.parent)
+    ss.S = zero(T)
+    ss.CS = zero(T)
+    ss.CCS = zero(T)
+    ss.S2 = zero(T)
+    ss.CS2 = zero(T)
+    ss.CCS2 = zero(T)
 end
 
 
 
-function lastMeasure(::Type{T}, ss::Naive{U})::SimulationMeasure{T}  where {T,U}
-    mean = ss.A/ss.counter
-    return SimulationMeasure(ss.counter, T(mean), T(ss.Q/(ss.counter) - (mean-ss.K)^2))
+function lastMeasure(::Type{T}, ss::Naive{U})::SimulationMeasure{T} where {T,U}
+    mean = ss.A / ss.counter
+    return SimulationMeasure(ss.counter, T(mean), T(ss.Q / (ss.counter) - (mean - ss.K)^2))
 end
 
 function recordValue(ss::SimulationStatistics{T}, x) where {T}
-	updateStatistics(ss.algo, x)
-	if counter(ss) == ss.nextSampleSize
-		push!(ss.table, lastMeasure(T, ss.algo))
-		ss.nextSampleSize = 2*(counter(ss)+1) - 1
-	end
+    updateStatistics(ss.algo, x)
+    if counter(ss) == ss.nextSampleSize
+        push!(ss.table, lastMeasure(T, ss.algo))
+        ss.nextSampleSize = 2 * (counter(ss) + 1) - 1
+    end
 end
 
 function counter(ss::SimulationStatistics{T}) where {T}
-	return counter(ss.algo)
+    return counter(ss.algo)
 end
 function counter(ss::Union{Klein{T,U},Kahan{T,U}}) where {T,U}
-	return ss.parent.counter
+    return ss.parent.counter
 end
 
-function counter(ss::Union{Naive{T}, ChanLewis{T}, Ling{T}}) where {T}
-	return ss.counter
+function counter(ss::Union{Naive{T},ChanLewis{T},Ling{T}}) where {T}
+    return ss.counter
 end
 
 function simulationMeasures(ss::SimulationStatistics{T}) where {T}
@@ -278,7 +302,7 @@ function simulationMeasures(ss::SimulationStatistics{T}) where {T}
 end
 
 function Base.reset(ss::SimulationStatistics{T}) where {T}
-	reset(ss.algo)
-	ss.table = T[]
-	ss.nextSampleSize = 128 - 1
+    reset(ss.algo)
+    ss.table = T[]
+    ss.nextSampleSize = 128 - 1
 end
